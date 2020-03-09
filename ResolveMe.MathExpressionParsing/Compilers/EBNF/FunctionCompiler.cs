efbf2ba8bd8c4ea16b73394eced36af9ea9491e2
Gramli <Gramli@seznam.cz>
2020-03-09 21:52:35 +0100

    Editing FunctionCompiler

--- ResolveMe.MathExpressionParsing/Compilers/EBNF/FunctionCompiler.cs
+++ ResolveMe.MathExpressionParsing/Compilers/EBNF/FunctionCompiler.cs
@@ -1,7 +1,7 @@
 ﻿using Amy;
 using Amy.EBNF.EBNFItems;
 using ResolveMe.MathCompiler.ExpressionTokens;
-using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Text;
 
@@ -9,47 +9,48 @@ namespace ResolveMe.MathCompiler.Compilers.EBNF
 {
     public class FunctionCompiler : NonTerminal, ICompiler
     {
-        public FunctionCompiler(string name) 
+        public FunctionCompiler(string name)
             : base(name)
         {
         }
 
         public ICompileResult Compile(string value)
         {
-            if (IsExpression(value))
+            var structure = ExpressionStructure(value);
+            if (structure is null || structure.Any())
             {
-                var structure = FetchLastExpressionStructure();
                 string name = GetName(structure);
                 IEnumerable<IToken> arguments = GetArguments(structure);
                 return new FunctionToken(name, arguments);
             }
+        
             else
                 throw new Exception("Compile error");
-        }
+    }
 
-        private IEnumerable<IToken> GetArguments(IEnumerable<IFormalGrammarItem> structure)
+    private IEnumerable<IToken> GetArguments(IEnumerable<IExpressionItem> structure)
+    {
+        List<ICompileResult> arguments = new List<ICompileResult>();
+        foreach (var item in structure)
         {
-            List<ICompileResult> arguments = new List<ICompileResult>();
-            foreach(var item in structure)
+            if (item is ICompiler)
             {
-                if (item is ICompiler)
-                {
-                    ICompileResult result = ((ICompiler)item).Compile();
-                    arguments.Add(result);
-                }
-            }
-        }
-
-        private string GetName(IEnumerable<IFormalGrammarItem> structure)
-        {
-            StringBuilder result = new StringBuilder();
-            foreach(var item in structure)
-            {
-                if(item is NonTerminal)
-                {
-
-                }
+                ICompileResult result = ((ICompiler)item).Compile();
+                arguments.Add(result);
             }
         }
     }
+
+    private string GetName(IEnumerable<IExpressionItem> structure)
+    {
+        StringBuilder result = new StringBuilder();
+        foreach (var item in structure)
+        {
+            if (item is NonTerminal)
+            {
+
+            }
+        }
+    }
+}
 }
