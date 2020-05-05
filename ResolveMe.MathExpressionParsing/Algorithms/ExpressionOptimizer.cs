b7923cc702db6cc2a2aab24a72a6cab83904f9fe
Daniel Balcarek <Gramli@seznam.cz>
2020-05-05 21:53:44 +0200

    Optimizer implementation

--- ResolveMe.MathExpressionParsing/Algorithms/ExpressionOptimizer.cs
+++ ResolveMe.MathExpressionParsing/Algorithms/ExpressionOptimizer.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using System.Text;
 using ResolveMe.MathCompiler.ExpressionTokens;
 
@@ -6,9 +8,13 @@ namespace ResolveMe.MathCompiler.Algorithms
 {
     public class ExpressionOptimizer
     {
-        private readonly char _leftBracket = '(';
-        private readonly char _rightBracket = ')';
+        private const char _leftBracket = '(';
+        private const char _rightBracket = ')';
+        private const char _leftSquareBracket = '[';
+        private const char _rightSquareBracket = ']';
         private readonly uint _optimalExpressionLength;
+
+        private readonly Dictionary<string, List<IExpressionToken>> variableTokens;
 
         public ExpressionOptimizer(uint optimalExpressionLength)
         {
@@ -17,7 +23,7 @@ namespace ResolveMe.MathCompiler.Algorithms
 
         public IEnumerable<IExpressionToken> SplitLongExpression(string value)
         {
-            if (value.Length > this._optimalExpressionLength && !string.IsNullOrEmpty(value))
+            if (value.Length > this._optimalExpressionLength && !string.IsNullOrEmpty(value) || IsFunctionRecursion(value))
             {
                 var editedValue = TryRemoveOuterBrackets(value);
                 var expressions = new List<IExpressionToken>();
@@ -40,25 +46,47 @@ namespace ResolveMe.MathCompiler.Algorithms
             return new List<IExpressionToken>(1) { new RawToken(value) };
         }
 
+        private bool IsFunctionRecursion(string value)
+        {
+            var functionBracketes = 0;
+            for (var i = 1; i < value.Length; i++)
+            {
+                if (value[i].Equals(_leftBracket) &&
+                    !(MathDefinitions.OperatorDefinitions.ContainsKey(value[i - 1]) || value[i - 1].Equals(',')))
+                    functionBracketes++;
+            }
+
+            return functionBracketes >= 4;
+        }
+
         private IEnumerable<IExpressionToken> GetTokenTokens(IExpressionToken token)
         {
             var tokenStringValue = token.GetStringRepresentation();
 
             var expressions = new List<IExpressionToken>();
-            var brackets = 0;
+            var roundBrackets = 0;
+            var squareBrackets = 0;
 
             for (var i = 0; i < tokenStringValue.Length; i++)
             {
-                if (tokenStringValue[i].Equals(_leftBracket))
+                switch (tokenStringValue[i])
                 {
-                    brackets++;
-                }
-                else if (tokenStringValue[i].Equals(_rightBracket))
-                {
-                    brackets--;
+                    case _leftBracket:
+                        roundBrackets++;
+                        break;
+                    case _rightBracket:
+                        roundBrackets--;
+                        break;
+                    case _leftSquareBracket:
+                        squareBrackets++;
+                        break;
+                    case _rightSquareBracket:
+                        squareBrackets--;
+                        break;
                 }
 
-                if (MathDefinitions.OperatorDefinitions.ContainsKey(tokenStringValue[i]) && brackets == 0 && i != 0)
+                //spliting by operator
+                if (MathDefinitions.OperatorDefinitions.ContainsKey(tokenStringValue[i]) && roundBrackets == 0 && i != 0)
                 {
                     expressions.AddRange(SplitLongExpression(tokenStringValue[..i]));
                     expressions.Add(CreateOperatorToken(tokenStringValue[i]));
@@ -66,40 +94,35 @@ namespace ResolveMe.MathCompiler.Algorithms
                     break;
                 }
 
-                if (tokenStringValue[i].Equals(','))
+                //spliting by arguments
+                if (tokenStringValue[i].Equals(',') && squareBrackets == 0)
                 {
+                    var leftArgumentStartIndex = CountBracketsUntil(tokenStringValue, i, -1, 1);
+                    var leftArgument = tokenStringValue[leftArgumentStartIndex..^i];
+                    var rightArgumentEndIndex = CountBracketsUntil(tokenStringValue, i, 1, -1);
+                    var rightArgument = tokenStringValue[i..^rightArgumentEndIndex];
+                    var functionName = GetFunctionName(tokenStringValue, i);
+
+                    expressions.Add(new FunctionToken(functionName));
+                    expressions.Add(new LeftBracketToken());
+                    expressions.AddRange(SplitLongExpression(leftArgument));
+                    expressions.Add(new CommaToken());
+                    expressions.AddRange(SplitLongExpression(rightArgument));
+                    expressions.Add(new RightBracketToken());
 
 
-
-                    //var stringhBuilder = new StringBuilder();
-                    //var argumentsBracket = 0;
-                    //for (var j = i - 1; j >= 0; j--)
-                    //{
-                    //    if (tokenStringValue[j].Equals(_leftBracket))
-                    //    {
-                    //        expressions.Add(new LeftBracketToken());
-                    //        argumentsBracket++;
-                    //        if (argumentsBracket == 0)
-                    //        {
-                    //            expressions.Add(new FunctionToken()
-                    //            break;
-                    //        }
-                    //    }
-                    //    else if (tokenStringValue[j].Equals(_rightBracket))
-                    //    {
-                    //        expressions.Add(new RightBracketToken());
-                    //        argumentsBracket--;
-                    //    }
-                    //    else
-                    //    {
-                    //        stringhBuilder.Insert(0, tokenStringValue[j]);
-                    //    }
-                    //}
-                    //expressions.AddRange(SplitLongExpression(stringhBuilder.ToString()));
-                    //expressions.Add(new CommaToken());
-                    //expressions.AddRange(SplitLongExpression(tokenStringValue[(i + 1)..^1]));
-                    //expressions.Add(new RightBracketToken());
-                    //break;
+                    //create new string with replacement
+                }
+                //splitting by function
+                if (roundBrackets % 2 == 1)
+                {
+                    var lastChar = tokenStringValue[i - 1];
+                    if (!MathDefinitions.OperatorDefinitions.ContainsKey(lastChar) && !lastChar.Equals(','))
+                    {
+                        //read to brackets = 0
+                        //go left for name, end is left bracket or 0
+                        //go right end is right bracket and brackets %2 == 0
+                    }
                 }
             }
 
@@ -148,6 +171,44 @@ namespace ResolveMe.MathCompiler.Algorithms
 
             result.Add(new RawToken(value));
             return result;
+        }
+
+        private int CountBracketsUntil(string tokenStringValue, int actualPosition, int step, int endValue)
+        {
+            var functionBrackets = 0;
+            var result = 0;
+            for (var j = actualPosition; j < tokenStringValue.Length; j += step)
+            {
+                if (tokenStringValue[j].Equals(_leftBracket))
+                {
+                    functionBrackets++;
+                }
+                else if (tokenStringValue[j].Equals(_rightBracket))
+                {
+                    functionBrackets--;
+                }
+
+                if (functionBrackets != endValue) continue;
+                
+                result = j;
+                break;
+            }
+
+            return result;
+        }
+
+        private string GetFunctionName(string tokenStringValue, int actualPosition)
+        {
+            var start = 0;
+            for (var j = actualPosition; j >= 0; j--)
+            {
+                if(!tokenStringValue[j].Equals(_leftBracket)) continue;
+
+                start = j;
+                break;
+            }
+
+            return tokenStringValue[start..^actualPosition];
         }
 
         private OperatorToken CreateOperatorToken(char token)
